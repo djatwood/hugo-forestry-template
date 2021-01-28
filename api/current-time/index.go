46b06429eb9b6217eb154c445bfae121a5b8dc67
Daniel Atwood <daniel@atwood.io>
2021-01-28 16:18:43 -0500

    Removed hello world line from current time function

--- api/current-time/index.go
+++ api/current-time/index.go
@@ -8,6 +8,5 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	currentTime := time.Now().Format(time.RFC850)
-	fmt.Fprintln(w, "Hello, World!")
 	fmt.Fprintln(w, currentTime)
 }
